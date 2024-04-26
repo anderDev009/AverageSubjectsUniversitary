d27d3966775224426453c422106ad8ecdb785ffb
Ander <richardanderdev009@gmail.com>
2024-04-26 01:45:25 -0400

    Inyeccion de dependencias

---
+++ AvgUniversitary.Core.Application/ServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿
+using AvgUniversitary.Core.Application.Interfaces.Services;
+using AvgUniversitary.Core.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace AvgUniversitary.Core.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationLayer(this IServiceCollection services)
+        {
+
+            #region Mapper
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            #endregion
+            #region Services
+            services.AddTransient<ISubjectService, SubjectService>();
+            services.AddTransient<IUniversityService, UniversityService>();
+            #endregion
+
+        }
+    }
+}
---
+++ AvgUniversitaryAPI/Program.cs
@@ -0,0 +1,32 @@
+using AvgUniversitary.Core.Application;
+using Universitary.Infrastructure.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+//agregando la capa de aplicacion
+builder.Services.AddApplicationLayer();
+//agregando la capa de infraestructura
+builder.Services.AddInfrastructureLayer(builder.Configuration);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
