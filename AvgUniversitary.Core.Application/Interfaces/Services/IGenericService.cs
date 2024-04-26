6beea7959207d4e3f0f4ed476165d1616fba32e9
Ander <richardanderdev009@gmail.com>
2024-04-26 01:44:05 -0400

    Creacion de servicios genericos

---
+++ AvgUniversitary.Core.Application/Interfaces/Services/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace AvgUniversitary.Core.Application.Interfaces.Services
+{
+    public interface IGenericService<DtoAdd,DtoUpdate,DtoGet>
+       where DtoAdd : class
+       where DtoUpdate:class
+       where DtoGet :class
+    {
+        Task<List<DtoGet>> GetAllAsync();
+        Task<DtoGet> GetByIdAsync(int id);
+        Task UpdateAsync(DtoUpdate dtoUpdate, int id);
+        Task DeleteAsync(int id);
+        Task<DtoAdd> AddAsync(DtoAdd dtoAdd); 
+    }
+}
---
+++ AvgUniversitary.Core.Application/Services/GenericService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using AvgUniversitary.Core.Application.Interfaces.Repositories;
+using AvgUniversitary.Core.Application.Interfaces.Services;
+
+
+namespace AvgUniversitary.Core.Application.Services
+{
+    public class GenericService<DtoAdd, DtoUpdate, DtoGet,T> : IGenericService<DtoAdd, DtoUpdate, DtoGet>
+       where DtoAdd : class
+       where DtoUpdate : class
+       where DtoGet : class
+    {
+        private readonly IBaseRepository<T> _repository;
+        private readonly IMapper _mapper;
+        public GenericService(IBaseRepository<T> repository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repository = repository;
+        }
+        public async Task<DtoAdd> AddAsync(DtoAdd dtoAdd)
+        {
+            T entity = _mapper.Map<T>(dtoAdd);
+            entity = await _repository.Save(entity);
+            return _mapper.Map<DtoAdd>(entity);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _repository.Delete(id);
+        }
+
+        public async Task<List<DtoGet>> GetAllAsync()
+        {
+
+            List<DtoGet> entities = _mapper.Map<List<DtoGet>>(await _repository.GetAll());
+            return entities;
+        }
+
+        public async Task<DtoGet> GetByIdAsync(int id)
+        {
+            DtoGet entity = _mapper.Map<DtoGet>(await _repository.GetById(id));
+            return entity;
+        }
+
+        public async Task UpdateAsync(DtoUpdate dtoUpdate, int id)
+        {
+            T entity = _mapper.Map<T>(dtoUpdate);
+            await _repository.Update(entity, id);
+        }
+    }
+}
